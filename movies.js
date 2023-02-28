51557f1ef75b5cafcdc975d359deac6e174b852a
Juliane Cassidy <julianecassidy@gmail.com>
2023-02-27 18:59:23 -0700

    edits to movies

--- movies.js
+++ movies.js
@@ -4,11 +4,15 @@
 
 //   // ;
 // }
+const movies = [];
 
 $("#submit").click(function (event) {
   event.preventDefault();
   const favMovie = $("#film").val();
-  const movieRating = $("#rating").val();
+  const movieRating = Number($("#rating").val());
+  if (favMovie.length < 3) return;
+  if (movieRating < 0 || movieRating > 10) return;
+  movies.push({favMovie:favMovie, movieRating: movieRating});
   $("ul").append(
     `<li>${favMovie}: ${movieRating} <button class='delete'>Delete</button></li>`
   );
@@ -19,3 +23,12 @@ function deleteMovie(event) {
 }
 
 $("#fav-movies-list").on("click", ".delete", deleteMovie);
+
+// const movieList = [];
+// $('li').each(function(){
+//   movieList.push($(this).text());
+// });
+
+// var myArray = $("li").map(function() {
+//   return $(this).text();
+// }).get();
